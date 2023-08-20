5eb59287ae87bd8e694fd203111bca5103734f30
Robert <zakiev@me.com>
2023-08-20 20:28:06 +0200

    Update quote fetching logic
    Updated the logic of fetching Yahoo quotes by taking into account their latest restrictions

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "github.com/compoundinvest/invest-core/quote/moexapi/historicalquotes"
-
 func main() {
-	historicalquotes.FetchHistoricalQuotesFor("SBER")
+
 }
--- quote/yahooapi/YahooQuoteService.go
+++ quote/yahooapi/YahooQuoteService.go
@@ -3,7 +3,10 @@ package yahooapi
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"net/http/cookiejar"
+	"net/url"
 	"strings"
 
 	quote "github.com/compoundinvest/invest-core/quote/entity"
@@ -11,10 +14,48 @@ import (
 
 type SimpleQuote = quote.SimpleQuote
 
-func fetchQuotes(tickers []string) []YahooQuote {
-	quotesURL := "https://query1.finance.yahoo.com/v6/finance/quote?symbols=" + strings.Join((tickers), ",")
+const user_agent_key = "User-Agent"
+const user_agent_value = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/536.36 (KHTML, like Gecko) Chrome/58.0.2029.110 Safari/437.36"
 
-	response, err := http.Get(quotesURL)
+func FetchQuotes(tickers []string) []SimpleQuote {
+	quotes := []quote.SimpleQuote{}
+
+	yahooQuotes := fetchQuotes(tickers)
+	quotes = append(quotes, quote.ConvertToSimpleQuote(yahooQuotes)...)
+
+	return quotes
+}
+
+func fetchQuotes(tickers []string) []YahooQuote {
+	cookie := fetchAuthCookie()
+	crumb := fetchCrumb(cookie)
+
+	apiBase := "https://query2.finance.yahoo.com/v7/finance/quote?"
+	queryParams := url.Values{
+		"crumb":   {crumb},
+		"symbols": {strings.Join((tickers), ",")},
+	}
+	quotesURL := apiBase + queryParams.Encode()
+
+	request, err := http.NewRequest("GET", quotesURL, nil)
+	request.AddCookie(cookie)
+	request.Header.Set(user_agent_key, user_agent_value)
+	if err != nil {
+		fmt.Println("Unable to create a GET request using the following URL: ", quotesURL)
+	}
+
+	cookieJar, err := cookiejar.New(nil)
+	if err != nil {
+		fmt.Println("Unable to configure a cookie jar")
+	}
+	yahooCookie := &http.Cookie{
+		Name:  cookie.Name,
+		Value: cookie.Value,
+	}
+	request.AddCookie(yahooCookie)
+
+	client := http.Client{Jar: cookieJar}
+	response, err := client.Do(request)
 	if err != nil {
 		fmt.Println("Unable to fetch quotes from ", quotesURL, ". ", err)
 		return nil
@@ -27,11 +68,50 @@ func fetchQuotes(tickers []string) []YahooQuote {
 	return quotesDTO.QuoteResponse.Result
 }
 
-func FetchQuotes(tickers []string) []SimpleQuote {
-	quotes := []quote.SimpleQuote{}
+// Workaround to make sure we can fetch quotes via Yahoo API
+func fetchAuthCookie() *http.Cookie {
+	cookieURL := "https://fc.yahoo.com/"
+	response, err := http.Get(cookieURL)
+	if err != nil {
+		fmt.Println("Unable to fetch the auth cookie from ", cookieURL, ". ", err)
+	}
 
-	yahooQuotes := fetchQuotes(tickers)
-	quotes = append(quotes, quote.ConvertToSimpleQuote(yahooQuotes)...)
+	cookie := response.Cookies()[0]
+	return cookie
+}
 
-	return quotes
+func fetchCrumb(cookie *http.Cookie) string {
+	crumbURL := "https://query2.finance.yahoo.com/v1/test/getcrumb"
+	request, err := http.NewRequest("GET", crumbURL, nil)
+	request.AddCookie(cookie)
+	request.Header.Set(user_agent_key, user_agent_value)
+	if err != nil {
+		fmt.Println("Unable to create a GET request using the following URL: ", crumbURL)
+	}
+
+	cookieJar, err := cookiejar.New(nil)
+	if err != nil {
+		fmt.Println("Unable to configure a cookie jar")
+	}
+	yahooCookie := &http.Cookie{
+		Name:  cookie.Name,
+		Value: cookie.Value,
+	}
+	request.AddCookie(yahooCookie)
+
+	client := http.Client{Jar: cookieJar}
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println("Unable to fetch the crumb from ", crumbURL, ". ", err)
+		return ""
+	}
+	defer response.Body.Close()
+
+	responseData, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Unable to read the crumb from the response: ", response)
+	}
+	responseString := string(responseData)
+
+	return responseString
 }
