1a28bf3f1c1e21e6189493697cd89088b67fe69d
robertzakspb <zakiev@me.com>
2025-07-13 21:19:44 +0200

    added a break

--- quote/moexapi/MoexQuoteService.go
+++ quote/moexapi/MoexQuoteService.go
@@ -56,6 +56,7 @@ func fetchQuotes(securities []quote.Security) []MoexQuote {
 			for _, security := range securities {
 				if security.Ticker == ticker {
 					figi = security.Figi
+					break
 				}
 			}
 
