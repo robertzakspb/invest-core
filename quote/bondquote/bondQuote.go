94790c8b7574f898ac6b8af7ffdc302712d878c8
Robert Zakiev <zakievrobert78@gmail.com>
2026-03-19 17:58:56 +0100

    Started working on the bond quote fetcher

---
+++ quote/bondquote/bondQuote.go
@@ -0,0 +1,26 @@
+package bondquote
+
+import "time"
+
+type BondQuote interface {
+	QuoteAsPercentage() float64
+	Figi() string
+	Timestamp() time.Time
+}
+
+type TinkoffBondQuote struct {
+	quoteAsPercentage float64
+	figi              string
+}
+
+func (quote TinkoffBondQuote) QuoteAsPercentage() float64 {
+	return quote.quoteAsPercentage
+}
+
+func (quote TinkoffBondQuote) Figi() string {
+	return quote.figi
+}
+
+func (quote TinkoffBondQuote) Timestamp() time.Time {
+	return quote.Timestamp()
+}
---
+++ quote/bondquote/fetcher.go
@@ -0,0 +1,30 @@
+package bondquote
+
+import (
+	"context"
+	"fmt"
+
+	"opensource.tbank.ru/invest/invest-go/investgo"
+)
+
+func fetchQuotesForFigis(figis []string, config investgo.Config) ([]TinkoffBondQuote, error) {
+	client, err := investgo.NewClient(context.TODO(), config, nil)
+	if err != nil {
+		return []TinkoffBondQuote{}, err
+	}
+
+	mdService := client.NewMarketDataServiceClient()
+	if mdService == nil {
+		return []TinkoffBondQuote{}, fmt.Errorf("failed to initialize Tinkoff's market data service")
+	}
+
+	quotes, err := mdService.GetLastPrices(figis)
+	if err != nil {
+		return []TinkoffBondQuote{}, err
+	}
+	for _, quote := range quotes.LastPrices {
+		fmt.Println(quote)
+	}
+
+	return []TinkoffBondQuote{}, nil //FIXME
+}
