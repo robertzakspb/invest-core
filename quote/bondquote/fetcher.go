5e90d429c6bec73746be0e053aaf1d8c841e36eb
robertzakspb <zakiev@me.com>
2026-03-20 14:22:34 +0100

    Exported a method

--- quote/bondquote/fetcher.go
+++ quote/bondquote/fetcher.go
@@ -7,7 +7,7 @@ import (
 	"opensource.tbank.ru/invest/invest-go/investgo"
 )
 
-func fetchQuotesForFigis(figis []string, config investgo.Config) ([]TinkoffBondQuote, error) {
+func FetchQuotesForFigis(figis []string, config investgo.Config) ([]TinkoffBondQuote, error) {
 	client, err := investgo.NewClient(context.TODO(), config, nil)
 	if err != nil {
 		return []TinkoffBondQuote{}, err
